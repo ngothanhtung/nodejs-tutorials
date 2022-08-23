d43e22fecc3f6bf19c5cf999c8b51404b3b77164
Ngô Thanh Tùng <tungnt@softech.vn>
2022-08-23 19:42:14 +0700

    Update Code

---
+++ express-app/routes/products.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var router = express.Router();
+
+router.all('/', (req, res, next) => {
+  console.log('Accessing the secret section ...');
+  next(); // pass control to the next handler
+});
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  res.send('This is products router');
+});
+
+router.get('/type', function (req, res, next) {
+  res.send('type');
+});
+
+router.get('/kind', function (req, res, next) {
+  res.send('kind');
+});
+
+router.get('/search/:name/type/:type', (req, res) => {
+  const name = req.params.name;
+  const type = req.params.type;
+  console.log('Params: ', req.params);
+  const price = req.query.price;
+  console.log(price);
+  res.send('ab?cd');
+});
+
+router.post('/', function (req, res, next) {
+  const result = [
+    {
+      id: 1,
+      name: 'iPhone 13',
+    },
+    {
+      id: 2,
+      name: 'iPhone 11',
+    },
+  ];
+  res.jsonp({ user: 'tobi' });
+  // res.json(result);
+});
+
+router.post('/search', function (req, res, next) {
+  const data = req.body;
+  console.log(data);
+  // Code here ....
+  const result = [];
+  res.json(result);
+});
+
+// DOWNLOAD
+router.get('/download', function (req, res, next) {
+  // res.send('OK');
+  res.download('public/images/nature.jpeg');
+});
+
+module.exports = router;
