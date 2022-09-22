da793ab77b27aaf50c5d877039bebc7fddcfb098
Ngô Thanh Tùng <tungnt@softech.vn>
2022-09-22 20:48:18 +0700

    Update Categories

--- express-app/routes/categories.js
+++ express-app/routes/categories.js
@@ -1,21 +1,47 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const yup = require('yup');
 
-const { insertDocument, updateDocument, findDocument, findDocuments } = require('../helpers/MongoDbHelper');
+const { insertDocument, updateDocument, findDocument, findDocuments, deleteDocument } = require('../helpers/MongoDbHelper');
 const { validateSchema, categorySchema } = require('./schemas.yup');
 
-router.get('/', function (req, res, next) {
-  findDocuments({}, 'categories', { name: -1 }, 50, [])
-    .then((result) => {
-      res.json(result);
-    })
-    .catch((error) => {
-      res.status(500).json(error);
-    });
+const COLLECTION_NAME = 'categories';
+
+router.get('/', async (req, res) => {
+  try {
+    let query = {};
+    const results = await findDocuments(query, COLLECTION_NAME);
+    res.json({ ok: true, results });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.get('/products', function (req, res, next) {
+// TÌM THEO ID
+const validateGetById = () =>
+  validateSchema(
+    yup.object({
+      params: yup.object({
+        id: yup.string().required(),
+      }),
+    }),
+  );
+
+router.get('/:id', validateGetById, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await findDocument(id, COLLECTION_NAME);
+    if (result) {
+      res.json({ ok: true, result });
+    } else {
+      res.json({ ok: false, result });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get('/products', function (req, res) {
   findDocuments({}, 'categories', {}, 50, [
     {
       $lookup: {
@@ -40,103 +66,77 @@ router.get('/products', function (req, res, next) {
     });
 });
 
-router.get('/:id', function (req, res, next) {
-  const { id } = req.params;
-  findDocument(id, 'categories')
-    .then((result) => {
-      res.json(result);
-    })
-    .catch((error) => {
-      res.status(500).json(error);
-    });
+// THÊM MỚI
+router.post('/', validateSchema(categorySchema), async (req, res) => {
+  try {
+    const data = req.body;
+    const result = await insertDocument(data, COLLECTION_NAME);
+    res.status(201).json({ ok: true, result });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.get(
-  '/search/name',
+// SỬA
+router.patch('/:id', async (req, res) => {
+  try {
+    const data = req.body;
+    const { id } = req.params;
+    const result = await updateDocument(id, data, COLLECTION_NAME);
+    res.json({ ok: true, result });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// XÓA
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await deleteDocument(id, COLLECTION_NAME);
+    res.json({ ok: true, result });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// SEARCH BY NAME
+const validateSearchByName = () =>
   validateSchema(
     yup.object({
       query: yup.object({
         text: yup.string().required(),
       }),
     }),
-  ),
-  function (req, res, next) {
-    const { text } = req.query;
-    // Find documents where the address starts with the letter "S":
+  );
+router.get('/search/name', validateSearchByName, function (req, res) {
+  const { text } = req.query;
 
-    // QUERY
-    // const query = { name: text };
-    const query = { name: new RegExp(`^${text}`) };
+  // QUERY
+  // const query = { name: text };
+  const query = { name: new RegExp(`${text}`) };
+  // const query = { $text: { $search: 'toys' } };
 
-    // SORT
-    // const sort = { name: 1 };
-    const sort = { name: -1 };
+  // SORT
+  // const sort = { name: 1 };
+  const sort = { name: -1 };
 
-    // LIMIT
-    const limit = 50;
+  // LIMIT
+  const limit = 50;
 
-    // SKIP
-    const skip = 1;
+  // SKIP
+  const skip = 1;
 
-    // PROJECTION: which columns you need
-    const projection = { name: 1 };
+  // PROJECTION: which columns you need
+  const project = {};
 
-    findDocuments({}, 'categories', sort, limit, [], skip, projection)
-      .then((result) => {
-        res.json(result);
-      })
-      .catch((error) => {
-        console.log(error);
-        res.status(500).json(error);
-      });
-  },
-);
-
-// router.post('/insert', validateSchema(categorySchema), function (req, res, next) {
-//   // 1. OPEN
-//   MongoClient.connect(url, function (err, db) {
-//     // ERROR of CONNECT
-//     if (err) {
-//       res.status(500).json({ ok: false, error: err });
-//       return;
-//     }
-
-//     const dbo = db.db(dbName);
-//     const category = req.body;
-//     dbo.collection('categories').insertOne(category, function (err, result) {
-//       // ERROR of INSERT
-//       if (err) {
-//         res.status(500).json({ ok: false, error: err });
-//         return;
-//       }
-//       // OK
-//       res.json({ ok: true, result: result });
-//       db.close();
-//       return;
-//     });
-//   });
-// });
-
-router.post('/', validateSchema(categorySchema), function (req, res, next) {
-  const data = req.body;
-  insertDocument(data, 'categories')
+  findDocuments(query, COLLECTION_NAME, sort, limit, [], skip, project)
     .then((result) => {
-      res.status(200).json({ ok: true, result });
+      res.json(result);
     })
     .catch((error) => {
-      res.status(500).json({ ok: false, error });
-    });
-});
-
-router.patch('/:id', function (req, res, next) {
-  const data = req.body;
-  const { id } = req.params;
-  updateDocument(id, data, 'categories')
-    .then((result) => {
-      res.status(200).json({ ok: true, result });
-    })
-    .catch((error) => {
-      res.status(500).json({ ok: false, error });
+      console.log(error);
+      res.status(500).json(error);
     });
 });
 
