744a546a880c8f9351a7a9a1f99b9a7fc3f79009
Ngo Thanh Tung <tungnt@softech.vn>
2022-11-03 11:52:13 +0700

    Update

--- MongoDB/nodejs/models/Product.js
+++ MongoDB/nodejs/models/Product.js
@@ -9,9 +9,9 @@ const { Schema, model } = mongoose;
 
 const productSchema = new Schema({
   name: { type: String, required: true },
-  price: { type: Number, required: true, min: [0, 'Must be at least 0, got {VALUE}'] },
-  discount: { type: Number, min: 0, max: 100, required: false },
-  stock: { type: Number, min: 0, required: false },
+  price: { type: Number, required: true, min: 0, default: 0 },
+  discount: { type: Number, min: 0, max: 75, default: 0 },
+  stock: { type: Number, min: 0, default: 0 },
   categoryId: { type: Schema.Types.ObjectId, ref: 'Category', required: true },
   supplierId: { type: Schema.Types.ObjectId, ref: 'Supplier', required: true },
 });
