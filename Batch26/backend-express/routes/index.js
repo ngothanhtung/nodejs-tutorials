ad647c48bffaff86afab35241b3c9216045ad224
Ngô Thanh Tùng <tungnt@softech.vn>
2022-12-10 17:03:54 +0700

    Update

--- Batch26/backend-express/routes/index.js
+++ Batch26/backend-express/routes/index.js
@@ -10,4 +10,20 @@ router.get('/about', function (req, res, next) {
   res.send('About');
 });
 
+router.get('/file/download', (req, res, next) => {
+  res.download('public/files/poster.png');
+});
+
+router.get('/sendFile', (req, res, next) => {
+  res.sendFile('/public/files/poster.png', { root: './' });
+});
+
+router.get('/json', (req, res, next) => {
+  res.json({ message: 'Hello' });
+});
+
+router.get('/redirect', (req, res, next) => {
+  res.redirect('/json');
+});
+
 module.exports = router;
