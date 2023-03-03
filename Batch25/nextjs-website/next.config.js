8e552c27c816c08b19e30ad54dddd9158d060927
Ngo Thanh Tung <tungnt@softech.vn>
2023-03-03 19:17:29 +0700

    Update build

--- Batch25/nextjs-website/next.config.js
+++ Batch25/nextjs-website/next.config.js
@@ -2,7 +2,9 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
-  ignoreBuildErrors: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
