47457be0f67ce112f8990da9c6d5161f06ed6935
Ngo Thanh Tung <tungnt@softech.vn>
2023-09-09 09:20:52 +0700

    Update

--- Batch31/express-typescript/data-source.ts
+++ Batch31/express-typescript/data-source.ts
@@ -2,6 +2,13 @@ require('dotenv').config();
 import 'reflect-metadata';
 
 import { DataSource } from 'typeorm';
+import { Category } from './entities/category.entity';
+import { Supplier } from './entities/supplier.entity';
+import { Customer } from './entities/customer.entity';
+import { Employee } from './entities/employee.entity';
+import { Product } from './entities/product.entity';
+import { Order } from './entities/order.entity';
+import { OrderDetail } from './entities/order-details.entity';
 
 export const AppDataSource = new DataSource({
   type: 'mssql',
@@ -10,7 +17,8 @@ export const AppDataSource = new DataSource({
   username: 'developer',
   password: 'developer',
   database: 'TypeOrm',
-  entities: ['entities/**/*.entity{.ts,.js}', 'entities/**/*.schema{.ts,.js}'],
+  // entities: ['entities/**/*.entity{.ts,.js}', 'entities/**/*.schema{.ts,.js}'],
+  entities: [Category, Supplier, Customer, Employee, Product, Order, OrderDetail],
   synchronize: true,
   logging: false,
   options: {
