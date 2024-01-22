94147dbfd6bcee9233e74644aab77ac6e0ec4754
Ngo Thanh Tung <tungnt@softech.vn>
2024-01-22 20:03:08 +0700

    Update Session 05 - Part 1

--- Batch36/web/routes/customers.js
+++ Batch36/web/routes/customers.js
@@ -31,4 +31,161 @@ router.post('/', async function (req, res, next) {
   }
 });
 
+// ------------------------------------------------------------------------------------------------
+// QUESTIONS 4a
+// ------------------------------------------------------------------------------------------------
+// https://www.mongodb.com/docs/manual/reference/operator/query/
+router.get('/questions/4a', function (req, res, next) {
+  try {
+    const text = 'Hải Châu';
+    const query = { address: new RegExp(`${text}`) };
+    // address có chứa từ Hải Châu
+
+    Customer.find(query)
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        res.status(400).send({ message: err.message });
+      });
+  } catch (err) {
+    res.sendStatus(500);
+  }
+});
+
+// ------------------------------------------------------------------------------------------------
+// QUESTIONS 4b
+// ------------------------------------------------------------------------------------------------
+// https://www.mongodb.com/docs/manual/reference/operator/query/
+// http://localhost:3000/customers/questions/4b?address=hải châu
+router.get('/questions/4b', function (req, res, next) {
+  try {
+    const text = req.query.address;
+    const query = { address: new RegExp(`${text}`) };
+    // address có chứa từ Hải Châu
+
+    Customer.find(query)
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        res.status(400).send({ message: err.message });
+      });
+  } catch (err) {
+    res.sendStatus(500);
+  }
+});
+
+// ------------------------------------------------------------------------------------------------
+// QUESTIONS 5a
+// ------------------------------------------------------------------------------------------------
+// https://www.mongodb.com/docs/v6.0/reference/operator/aggregation/year/
+router.get('/questions/5a', function (req, res, next) {
+  try {
+    const query = {
+      $expr: {
+        $eq: [{ $year: '$birthday' }, 1990],
+      },
+    };
+
+    Customer.find(query)
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        res.status(400).send({ message: err.message });
+      });
+  } catch (err) {
+    res.sendStatus(500);
+  }
+});
+
+// ------------------------------------------------------------------------------------------------
+// QUESTIONS 5b
+// ------------------------------------------------------------------------------------------------
+// https://www.mongodb.com/docs/v6.0/reference/operator/aggregation/year/
+// http://localhost:3000/customers/questions/5b?year=1990
+router.get('/questions/5b', function (req, res, next) {
+  try {
+    const year = req.query.year;
+
+    const query = {
+      $expr: {
+        $eq: [{ $year: '$birthday' }, parseInt(year)],
+      },
+    };
+
+    Customer.find(query)
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        res.status(400).send({ message: err.message });
+      });
+  } catch (err) {
+    res.sendStatus(500);
+  }
+});
+
+// ------------------------------------------------------------------------------------------------
+// QUESTIONS 6a
+// ------------------------------------------------------------------------------------------------
+// https://www.mongodb.com/docs/v6.0/reference/operator/aggregation/month/
+// https://www.mongodb.com/docs/v6.0/reference/operator/aggregation/dayOfMonth/
+// https://www.mongodb.com/docs/manual/reference/operator/query/
+router.get('/questions/6a', function (req, res, next) {
+  try {
+    const today = new Date();
+    const eqDay = { $eq: [{ $dayOfMonth: '$birthday' }, { $dayOfMonth: today }] };
+    const eqMonth = { $eq: [{ $month: '$birthday' }, { $month: today }] };
+
+    const query = {
+      $expr: {
+        $and: [eqDay, eqMonth],
+      },
+    };
+
+    Customer.find(query)
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        res.status(400).send({ message: err.message });
+      });
+  } catch (err) {
+    res.sendStatus(500);
+  }
+});
+
+// ------------------------------------------------------------------------------------------------
+// QUESTIONS 6a
+// ------------------------------------------------------------------------------------------------
+// https://www.mongodb.com/docs/v6.0/reference/operator/aggregation/month/
+// https://www.mongodb.com/docs/v6.0/reference/operator/aggregation/dayOfMonth/
+// https://www.mongodb.com/docs/manual/reference/operator/query/
+// http://localhost:9000/customers/questions/6b?birthdayMonth=01&birthdayDay=22
+router.get('/questions/6b', function (req, res, next) {
+  try {
+    const birthday = new Date(1990, parseInt(req.query.birthdayMonth) - 1, parseInt(req.query.birthdayDay) + 1);
+    const eqDay = { $eq: [{ $dayOfMonth: '$birthday' }, { $dayOfMonth: birthday }] };
+    const eqMonth = { $eq: [{ $month: '$birthday' }, { $month: birthday }] };
+
+    const query = {
+      $expr: {
+        $and: [eqDay, eqMonth],
+      },
+    };
+
+    Customer.find(query)
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        res.status(400).send({ message: err.message });
+      });
+  } catch (err) {
+    res.sendStatus(500);
+  }
+});
+
 module.exports = router;
--- Batch36/web/routes/products.js
+++ Batch36/web/routes/products.js
@@ -2,7 +2,6 @@ const yup = require('yup');
 var express = require('express');
 var router = express.Router();
 
-const { default: mongoose } = require('mongoose');
 const { Product } = require('../models');
 const ObjectId = require('mongodb').ObjectId;
 
@@ -52,6 +51,159 @@ router.post('/', async function (req, res, next) {
     .catch((err) => {
       return res.status(400).json({ type: err.name, errors: err.errors, provider: 'yup' });
     });
+});
+
+router.get('/questions/1a', function (req, res, next) {
+  try {
+    // Get all products with discount <= 10%
+    let query = { discount: { $lte: 10 } };
+    Product.find(query)
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        res.status(400).send({ message: err.message });
+      });
+  } catch (err) {
+    res.sendStatus(500);
+  }
+});
+
+router.get('/questions/1b', function (req, res, next) {
+  try {
+    // Get all products with discount <= 10%
+    let query = { discount: { $lte: 10 } };
+    Product.find(query)
+      .populate('category')
+      .populate('supplier')
+      .lean({ virtuals: true })
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        res.status(400).send({ message: err.message });
+      });
+  } catch (err) {
+    res.sendStatus(500);
+  }
+});
+
+// http://localhost:3000/products/questions/1c?discount=10
+router.get('/questions/1c', function (req, res, next) {
+  try {
+    const discount = req.query.discount;
+    // Get all products with discount <= n%
+    const query = { discount: { $lte: discount } };
+    Product.find(query)
+      .populate('category')
+      .populate('supplier')
+      .lean({ virtuals: true })
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        res.status(400).send({ message: err.message });
+      });
+  } catch (err) {
+    res.sendStatus(500);
+  }
+});
+
+// http://localhost:3000/products/questions/1d?discountMin=10&discountMax=20
+router.get('/questions/1d', function (req, res, next) {
+  try {
+    const { discountMin, discountMax } = req.query;
+
+    // Get all products with discount <= n%
+    const query = { discount: { $gte: parseFloat(discountMin), $lte: parseFloat(discountMax) } };
+    Product.find(query)
+      .populate('category')
+      .populate('supplier')
+      .lean({ virtuals: true })
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        res.status(400).send({ message: err.message });
+      });
+  } catch (err) {
+    res.sendStatus(500);
+  }
+});
+
+// http://localhost:3000/products/questions/1d?discountMin=10&discountMax=20&priceMin=1000&priceMax=2000
+router.get('/questions/1e', function (req, res, next) {
+  try {
+    const { discountMin, discountMax, priceMin, priceMax } = req.query;
+
+    const query = {
+      discount: { $gte: parseFloat(discountMin), $lte: parseFloat(discountMax) },
+      price: { $gte: parseFloat(priceMin), $lte: parseFloat(priceMax) },
+    };
+
+    // const query = {
+    //   $and: [{ discount: { $gte: parseFloat(discountMin), $lte: parseFloat(discountMax) } }, { price: { $gte: parseFloat(priceMin), $lte: parseFloat(priceMax) } }],
+    // };
+
+    Product.find(query)
+      .populate('category')
+      .populate('supplier')
+      .lean({ virtuals: true })
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        res.status(400).send({ message: err.message });
+      });
+  } catch (err) {
+    res.sendStatus(500);
+  }
+});
+
+router.get('/questions/3a', async (req, res, next) => {
+  try {
+    // let finalPrice = price * (100 - discount) / 100;
+
+    const s = { $subtract: [100, '$discount'] }; // (100 - 5)
+    const m = { $multiply: ['$price', s] }; // price * 95
+    const d = { $divide: [m, 100] }; // price * 95 / 100
+
+    const query = { $expr: { $lte: [d, parseFloat(1000)] } };
+    Product.find(query)
+      .lean({ virtuals: true })
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// http://localhost:3000/products/questions/3b?price=1000
+router.get('/questions/3b', async (req, res, next) => {
+  try {
+    // let finalPrice = price * (100 - discount) / 100;
+    const price = req.query.price;
+
+    const s = { $subtract: [100, '$discount'] }; // (100 - 5)
+    const m = { $multiply: ['$price', s] }; // price * 95
+    const d = { $divide: [m, 100] }; // price * 95 / 100
+
+    let query = { $expr: { $lte: [d, parseFloat(price)] } };
+    Product.find(query)
+      .lean({ virtuals: true })
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
