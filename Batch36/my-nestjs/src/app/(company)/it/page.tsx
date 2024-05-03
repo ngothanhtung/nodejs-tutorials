71050d982fb2e3842c76f9733c5eb7610ced98e0
Ngo Thanh Tung <tungnt@softech.vn>
2024-05-03 10:32:53 +0700

    Update

--- Batch36/my-nestjs/src/app/(company)/it/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-type Props = {};
-
-export default function It({}: Props) {
-  return <div>It</div>;
-}
--- Batch36/my-nestjs/src/app/(company)/sales/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-type Props = {};
-
-export default function Sales({}: Props) {
-  return <div>Sales</div>;
-}
